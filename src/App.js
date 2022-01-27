6280680bec1dd9337792f1d077a3fede64345985
Melissa Guachun <1212malka@gmail.com>
2022-01-26 23:44:27 -0500

    started fetch and lay out state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MovieCard from './MovieCard';
 
-function App() {
+const App = () => {
+  const [film, setFilms] = useState([]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Studio Ghibli</h1>
+      <MovieCard setter={setFilms}/>
+
     </div>
   );
 }
