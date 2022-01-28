4df1e690553feeb5e043e9532389091dc9d92c57
Melissa Guachun <1212malka@gmail.com>
2022-01-27 21:07:33 -0500

    movie description

---
+++ src/MovieInfo.js
@@ -0,0 +1,22 @@
+import React from "react";
+export default function MovieInfo({ film }) {
+    if (!film) {
+        return <div>Choose a Film</div>;
+    }
+    return (
+        <div>
+            <h2>{film.title}</h2>
+            <h3>{film.description}</h3>
+            <div>
+            <h2>Director: {film.director}</h2>
+            </div>
+            <div>
+            <h2>Producer: {film.producer}</h2>
+            </div>
+            <div>
+            <h2>Released: {film.release_date}</h2>
+            </div>
+        </div>
+        
+    )
+}
