dec47fcd2623961cc66e85bf3af10065309610e4
Melissa Guachun <1212malka@gmail.com>
2022-01-27 21:07:42 -0500

    plugging in movieinfo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
+import MovieInfo from './MovieInfo';
 
 const App = () => {
   const [film, setFilms] = useState([]);
@@ -10,6 +11,7 @@ const App = () => {
     <div className="App">
       <h1>Studio Ghibli</h1>
       <MovieCard setter={setFilms}/>
+      <MovieInfo film={film}/>
 
     </div>
   );
