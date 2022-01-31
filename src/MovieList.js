fe65e344867b11bf2bb7495b304bbf3a2c7eaff7
Melissa Guachun <1212malka@gmail.com>
2022-01-31 17:23:54 -0500

    name change from card to list

---
+++ src/MovieList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+export default function MovieList({ setter }) {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        const url = "https://ghibliapi.herokuapp.com/films";
+
+        fetch(url)
+            .then(response => response.json())
+            .then(response => setFilms(response))
+    }, []);
+
+    return (
+        <div>
+
+            {films && films.map(film => (
+                <div key={film.id} onClick={e => setter(film)}>
+                    {film.title}
+                    
+                </div>
+            ))}
+
+
+        </div>
+    )
+}
