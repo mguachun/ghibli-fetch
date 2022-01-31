9cf6fc2263f3f6c42b81408d8c54181550288cb0
Melissa Guachun <1212malka@gmail.com>
2022-01-31 17:11:08 -0500

    formatted div

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -7,19 +7,19 @@ export default function MovieCard({ setter }) {
         const url = "https://ghibliapi.herokuapp.com/films";
 
         fetch(url)
-        .then(response => response.json())
-        .then(response => setFilms(response))
+            .then(response => response.json())
+            .then(response => setFilms(response))
     }, []);
 
     return (
         <div>
 
-                {films && films.map(film => (
-                    <div key={film.id} onClick={e => setter(film)}>
-                        {film.title}
-                    </div>
-                ))}
-          
+            {films && films.map(film => (
+                <div key={film.id} onClick={e => setter(film)}>
+                    {film.title}
+                </div>
+            ))}
+
 
         </div>
     )
