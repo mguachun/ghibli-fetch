dc98eb0dd56477da4ebd87e138efce32d82af6b4
Melissa Guachun <1212malka@gmail.com>
2022-01-31 18:37:13 -0500

    organizing info

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -5,20 +5,22 @@ export default function MovieInfo({ film }) {
     }
     return (
         <div>
+            
             <h2>{film.title}</h2>
-            <h3>{film.description}</h3>
-            <div>
+
+            <img src={film.image}></img>
+
+            <h3 className="description">{film.description}</h3>
+         
             <h2>{film.director}</h2>
-            </div>
-            <div>
+           
             <h2>{film.producer}</h2>
-            </div>
-            <div>
+          
             <h2>{film.release_date}</h2>
-            </div>
-            <div>
-             <img src={film.image}></img>
-            </div>
+          
+    
+          
+       
 
         </div>
         
