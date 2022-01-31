f0579268043c7ffc54556b635f2bd1a6935f1b4a
Melissa Guachun <1212malka@gmail.com>
2022-01-31 17:10:52 -0500

    possible year filter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
 import MovieInfo from './MovieInfo';
+// import YearFilter from './YearFilter';
 
 const App = () => {
   const [film, setFilms] = useState([]);
 
   // make a filter for years movie was made
 
+
   return (
     <div className="App">
       <h1>Studio Ghibli</h1>
-      <MovieCard setter={setFilms}/>
-      <MovieInfo film={film}/>
+      {/* <YearFilter /> */}
+      <MovieCard setter={setFilms} />
+      <MovieInfo film={film} />
+
     </div>
   );
 }
