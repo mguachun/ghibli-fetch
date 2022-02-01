0ab60e534c4d871cae19a45f22ef059f524497b8
Melissa Guachun <1212malka@gmail.com>
2022-01-31 19:30:57 -0500

    changed classname/deleted

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,16 +1,17 @@
 import React from "react";
+
 export default function MovieInfo({ film }) {
     if (!film) {
         return <div>Choose a Film</div>;
     }
     return (
-        <div>
+        <div className="info">
             
             <h2>{film.title}</h2>
 
             <img src={film.image}></img>
 
-            <h3 className="description">{film.description}</h3>
+            <h3>{film.description}</h3>
          
             <h2>{film.director}</h2>
            
