bc431e83e522b5eb7ca6df9db8308ba6d8ccffdb
Melissa Guachun <1212malka@gmail.com>
2022-04-04 12:51:26 -0400

    added div with different classname

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,24 +1,29 @@
 import React from "react";
 
+
 export default function MovieInfo({ film }) {
     if (!film) {
         return <div>Choose a Film</div>;
     }
     return (
         <div className="info">
-            
+
             <h1>{film.title}</h1>
             <h2>{film.original_title}</h2>
 
             <img src={film.image}></img>
 
-            <h3 className="description">{film.description}</h3>
+            <div className="description">
+            <h3 >{film.description}</h3>
          
-            <h2 className="description">{film.director}</h2>
-           
-            <h2 className="description">{film.producer}</h2>
-          
-            <h2 className="description">{film.release_date}</h2>
+            <h2>{film.director}</h2>
+            
+            <h2>{film.producer}</h2>
+        
+            <h2>{film.release_date}</h2>
+
+            </div>
+
 
            
           <br></br>
